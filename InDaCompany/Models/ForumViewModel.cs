b622f248b3814aa198408167378e133ac286d9be
Viosil7 <silvio.bozzolato@outlook.com>
2025-02-12 17:13:29 +0100

    forum

---
+++ InDaCompany/Models/ForumViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace InDaCompany.Models;
+
+public class ForumViewModel
+{
+    public List<Forum> Forum { get; set; }
+    public List<ThreadForum> Threads { get; set; }
+    public List<MessaggioThread> Messages { get; set; }
+}
