05ab23522f3ef9d1edb9bd06cc5d73b49ffc1c71
calon96 <noivoi.tu5@gmail.com>
2025-02-16 08:46:59 +0100

    sistemato namespace models

--- InDaCompany/Models/ThreadForum.cs
+++ InDaCompany/Models/ThreadForum.cs
@@ -1,27 +1,29 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
-public class ThreadForum
+namespace InDaCompany.Models
 {
-    public int ID { get; set; }
+    public class ThreadForum
+    {
+        public int ID { get; set; }
 
-    [Required(ErrorMessage = "Il campo Titolo è obbligatorio")]
-    [StringLength(255, ErrorMessage = "Il Titolo non può superare i 255 caratteri")]
-    public string Titolo { get; set; } = null!;
+        [Required(ErrorMessage = "Il campo Titolo è obbligatorio")]
+        [StringLength(255, ErrorMessage = "Il Titolo non può superare i 255 caratteri")]
+        public string Titolo { get; set; } = null!;
 
-    [Required(ErrorMessage = "Il campo Testo è obbligatorio")]
-    public string Testo { get; set; } = null!;
+        [Required(ErrorMessage = "Il campo Testo è obbligatorio")]
+        public string Testo { get; set; } = null!;
 
-    [Required(ErrorMessage = "Il campo Forum è obbligatorio")]
-    public int ForumID { get; set; }
+        [Required(ErrorMessage = "Il campo Forum è obbligatorio")]
+        public int ForumID { get; set; }
 
-    [Required(ErrorMessage = "Il campo Autore è obbligatorio")]
-    public int AutoreID { get; set; }
+        [Required(ErrorMessage = "Il campo Autore è obbligatorio")]
+        public int AutoreID { get; set; }
 
-    public byte[]? Immagine { get; set; }
+        public byte[]? Immagine { get; set; }
 
-    public DateTime DataCreazione { get; set; } = DateTime.Now;
+        public DateTime DataCreazione { get; set; } = DateTime.Now;
 
-    public string TestoAnteprima => Testo.Length > 100 ?
-        Testo.Substring(0, 97) + "..." :
-        Testo;
+        public string TestoAnteprima => Testo.Length > 100 ?
+            Testo.Substring(0, 97) + "..." :
+            Testo;
+    }
 }
